0f4f298865aac32f63207970ef7a0b0ded6dfe78
Osheenjain646 <osheenjain646@gmail.com>
2026-02-01 03:03:43 +0530

    DSA

--- untitled/src/LeetCode/Contest/Biweekly175/Q2.java
+++ untitled/src/LeetCode/Contest/Biweekly175/Q2.java
@@ -2,29 +2,59 @@ package LeetCode.Contest.Biweekly175;
 import java.math.BigInteger;
 public class Q2 {
 
-    public static int nonPositive(int[] nums , int k){
-        int operations=0;
-        for (int num:nums){
-            while (num>0){
-                num-=k;
-                operations++;
+    public static int minimumKOptimal(int[] nums) {
+        // Using binary search
+        int n = nums.length;
+        int start = 1;
+        int end = n;         // as minimum operation must be nums.length
+        for (int num : nums) end = Math.max(end, num);
+
+        int ans = end;
+        while (start<=end){
+            int mid = start+(end-start)/2;
+            long operations=0;
+            for (int x : nums){
+                if (mid!=0){
+                    operations+=(x+mid-1)/mid;
+                }
+                if (operations>(long) mid*mid) break;
+            }
+            if (operations<=mid*mid){
+                ans=mid;
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
             }
         }
-        return operations;
+        return ans;
     }
 
-    public static int minimumK(int[] nums){
-        int minimum = Integer.MAX_VALUE;
-        for (int num : nums) {
-            int operations=nonPositive(nums,num);
-            if (operations<=(num*num))
-                minimum = Math.min(minimum,num);
-        }
-        return minimum;
-    }
+// Brute Force
+//    public static long nonPositive(int[] nums, int k) {
+//        long ops = 0;
+//        for (int num : nums) {
+//            ops += (num + k - 1) / k; // ceil function
+//        }
+//        return ops;
+//    }
+//
+//    public static int minimumKBruteForce(int[] nums) {
+//        int ans = Integer.MAX_VALUE;
+//
+//        for (int k = 1; k <= 100000; k++) {
+//            long ops = nonPositive(nums, k);
+//            if (ops <= (long) k * k) {
+//                ans = k;
+//                break;
+//            }
+//        }
+//        return ans == Integer.MAX_VALUE ? -1 : ans;
+//    }
 
     static void main() {
-        int[] nums = new int[]{3,7,5};
-        int k = minimumK(nums);
+        int[] nums = {3,7,5};
+        int k = minimumKOptimal(nums);
+        System.out.println(k);
     }
 }
