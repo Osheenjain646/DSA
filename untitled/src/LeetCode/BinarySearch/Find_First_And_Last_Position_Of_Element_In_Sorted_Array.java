6c2ddf69688c8dd186392010079a08edaea9a3ee
Osheenjain646 <osheenjain646@gmail.com>
2026-02-13 00:07:29 +0530

    Binary Search

---
+++ untitled/src/LeetCode/BinarySearch/Find_First_And_Last_Position_Of_Element_In_Sorted_Array.java
@@ -0,0 +1,30 @@
+package LeetCode.BinarySearch;
+
+public class Find_First_And_Last_Position_Of_Element_In_Sorted_Array {
+
+    public static int[] searchRangeBetter(int[] nums , int target){
+
+    }
+
+    public static int[] searchRangeBruteForce(int[] nums , int target){
+        int first=-1;
+        int last=-1;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]==target){
+                first=i;
+                break;
+            }
+        }
+        for (int j=nums.length-1; j>=0; j--){
+            if (nums[j]==target){
+                last=j;
+                break;
+            }
+        }
+        return new int[]{first,last};
+    }
+
+    public static void main(String[] args){
+
+    }
+}
