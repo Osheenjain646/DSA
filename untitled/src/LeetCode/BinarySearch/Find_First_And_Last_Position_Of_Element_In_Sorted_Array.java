5b7ca8be04c80b6f38dc9535d4a4b87b9900bc02
Osheenjain646 <osheenjain646@gmail.com>
2026-02-13 14:48:41 +0530

    Binary search with upper and lower bound

--- untitled/src/LeetCode/BinarySearch/Find_First_And_Last_Position_Of_Element_In_Sorted_Array.java
+++ untitled/src/LeetCode/BinarySearch/Find_First_And_Last_Position_Of_Element_In_Sorted_Array.java
@@ -3,7 +3,40 @@ package LeetCode.BinarySearch;
 public class Find_First_And_Last_Position_Of_Element_In_Sorted_Array {
 
     public static int[] searchRangeBetter(int[] nums , int target){
-        return new int[]{};
+        int i=0;
+        int j=nums.length;
+        int upperBound=-1;
+        int lowerBound=-1;
+        while (i<j){
+            int mid=i+(j-i)/2;
+            if (nums[mid]>=target){
+                j=mid;
+            }
+            else {
+                i=mid+1;
+            }
+        }
+        if (i<nums.length&&nums[i]==target){
+            lowerBound=i;
+        }
+
+        i=0;
+        j=nums.length;
+        while (i<j){
+            int mid=i+(j-i)/2;
+            if (nums[mid]>target){
+                j=mid;
+            }
+            else {
+                i=mid+1;
+            }
+        }
+
+        if (i>0&&i<=nums.length&&nums[i-1]==target){
+            upperBound=i-1;
+        }
+
+        return new int[]{lowerBound,upperBound};
     }
 
     public static int[] searchRangeBruteForce(int[] nums , int target){
