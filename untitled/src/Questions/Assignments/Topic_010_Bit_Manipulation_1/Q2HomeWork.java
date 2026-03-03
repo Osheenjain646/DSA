828d5b4ff78b6e1abdb8afb9f69c37c6dd2aa597
Osheenjain646 <osheenjain646@gmail.com>
2026-03-03 19:19:29 +0530

    Practice

---
+++ untitled/src/Questions/Assignments/Topic_010_Bit_Manipulation_1/Q2HomeWork.java
@@ -0,0 +1,71 @@
+package Questions.Assignments.Topic_010_Bit_Manipulation_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Q2HomeWork {
+
+    /*
+    ###Problem Description
+
+You have an array A with N elements. We have two types of operation available on this array :
+We can split an element B into two elements, C and D, such that B = C + D.
+We can merge two elements, P and Q, to one element, R, such that R = P ^ Q i.e., XOR of P and Q.
+You have to determine whether it is possible to convert array A to size 1, containing a single element equal to 0 after several splits and/or merge?
+###Problem Constraints
+
+1 <= N <= 100000
+
+1 <= A[i] <= 10^6
+###Input Format
+
+The first argument is an integer array A of size N.
+###Output Format
+
+Return "Yes" if it is possible otherwise return "No".
+###Example Input ####Input 1:
+
+A = [9, 17]
+####Input 2:
+
+A = [1]
+###Example Output ####Output 1:
+
+Yes
+####Output 2:
+
+No
+###Example Explanation ####Explanation 1:
+
+Following is one possible sequence of operations -
+1) Merge i.e 9 XOR 17 = 24
+2) Split 24 into two parts each of size 12
+3) Merge i.e 12 XOR 12 = 0
+As there is only 1 element i.e 0. So it is possible.
+####Explanation 2:
+
+There is no possible way to make it 0.
+    */
+
+    public static String InterestingArray(ArrayList<Integer> nums){
+        if (nums.size()==1&&nums.get(0)%2==0){
+            return "Yes";
+        }
+
+        int ans =0;
+        for (Integer number : nums){
+            if (number%2!=0){
+                ans=ans^number;
+                if (ans%2==0){
+                    ans=0;
+                }
+            }
+        }
+        return ans==0 ? "Yes" : "No";
+    }
+
+    public static void main(String[] args){
+        System.out.println(InterestingArray(new ArrayList<>(Arrays.asList(9,17))));
+        System.out.println(InterestingArray(new ArrayList<>(Arrays.asList(1,2))));
+    }
+}
---
+++ untitled/src/Questions/Assignments/Topic_010_Bit_Manipulation_1/Q3HomeWork.java
@@ -0,0 +1,54 @@
+package Questions.Assignments.Topic_010_Bit_Manipulation_1;
+
+public class Q3HomeWork {
+
+    public static int reverseBitsOptimal(int n){
+        int res =0;
+        for (int i=0; i<32; i++){
+            if ((n&(1<<i))!=0){
+                res|=(1<<(31-i));
+            }
+        }
+        return res;
+    }
+
+    public static int reverseBitsBetter(int n){
+        int num=0;
+        for (int i=31; i>=0; i--){
+            if ((n&1)==1){
+                num+=1<<i;
+            }
+            n=n>>1;
+        }
+        return num;
+    }
+
+    public static int reverseBitsBruteForce(int n){
+        StringBuilder binaryNumber = new StringBuilder();
+        for (int i=0; i<32; i++){
+            if ((n&(1<<i))!=0){
+                binaryNumber.append("1");
+            }else {
+                binaryNumber.append("0");
+            }
+        }
+
+        int res =0;
+        String reversedNumber = binaryNumber.reverse().toString();
+        for (int i=0; i<32; i++){
+            if (reversedNumber.charAt(i)=='1'){
+                res|=(1<<i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        System.out.println(reverseBitsBruteForce(43261596));
+        System.out.println(reverseBitsBruteForce(2147483644));
+        System.out.println(reverseBitsBetter(43261596));
+        System.out.println(reverseBitsBetter(2147483644));
+        System.out.println(reverseBitsOptimal(43261596));
+        System.out.println(reverseBitsOptimal(2147483644));
+    }
+}
